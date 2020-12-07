c3d272733d8db0fcc10b1bb310e6594a77a67ac6
Delaney Sylvans <delaneysylvans@gmail.com>
2020-12-07 12:46:57 +0100

    Update DB Conn for Heroku

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://yocbkgrnleuupg:699b1ed4af6f1e8bbfec91d179364a73ba945110660594ad23c7bb702af5e637@ec2-23-20-205-19.compute-1.amazonaws.com:5432/dbm2rgfv80t1gu');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,12 +68,17 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '5432'),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
+            'host'  => $DATABASE_URL['host'],
+            'port'  => $DATABASE_URL['port'],
+            'database'  => ltrim($DATABASE_URL['path'], '/'),
+            'username'  => $DATABASE_URL['user'],
+            'password'  => $DATABASE_URL['pass'],
+            'charset' => 'utf8mb4',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
