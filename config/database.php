67b8e8ac87627dcaa313e47f7b2243e34bd2f662
Delaney Sylvans <delaneysylvans@gmail.com>
2020-12-07 19:16:04 +0100

    Back to MySQL

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url('postgres://yocbkgrnleuupg:699b1ed4af6f1e8bbfec91d179364a73ba945110660594ad23c7bb702af5e637@ec2-23-20-205-19.compute-1.amazonaws.com:5432/dbm2rgfv80t1gu');
+$DATABASE_URL = parse_url('mysql://bb38b4da6639b0:e41a2d70@us-cdbr-east-02.cleardb.com/heroku_b40a2b0650faddf?reconnect=true');
 
 return [
 
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -48,11 +48,16 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '3306'),
+            // 'database' => env('DB_DATABASE', ''),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
+            'host'  => $DATABASE_URL['host'],
+            'port'  => $DATABASE_URL['port'],
+            'database'  => ltrim($DATABASE_URL['path'], '/'),
+            'username'  => $DATABASE_URL['user'],
+            'password'  => $DATABASE_URL['pass'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
