430d4a6390c3c2c74110df9245a7761fb1d8fd3b
Delaney Sylvans <delaneysylvans@gmail.com>
2021-02-09 14:06:32 +0100

    Web CORS

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization, Content-Type, Accept');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
